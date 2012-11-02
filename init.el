15b5f0890edbd035cb4b3e822721bb4a84266a69
Piotr Kowalski <piotr.kowalski@me.com>
2012-11-02 19:26:36 +0100

    Configures ibuffer groups.

--- init.el
+++ init.el
@@ -140,6 +140,41 @@
 ;; use ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
+;; configure groups
+(setq ibuffer-saved-filter-groups
+      (quote (("default"
+               ("dired" (mode . dired-mode))
+               ("ruby" (or
+                        (name . "\\.html\\.erb\\'")
+                        (mode . ruby-mode)))
+               ("javascript" (or
+                              (name . "\\.jst\\.ejs\\'")
+                              (name . "\\.jst\\.eco\\'")
+                              (mode . coffee-mode)
+                              (mode . js-mode)))
+               ("erc" (mode . erc-mode))
+               ("planner" (or
+                           (name . "^\\*Calendar\\*$")
+                           (name . "^diary$")
+                           (mode . muse-mode)))
+               ("emacs" (or
+                         (name . "^\\*scratch\\*$")
+                         (name . "^\\*Messages\\*$")))
+               ("gnus" (or
+                        (mode . message-mode)
+                        (mode . bbdb-mode)
+                        (mode . mail-mode)
+                        (mode . gnus-group-mode)
+                        (mode . gnus-summary-mode)
+                        (mode . gnus-article-mode)
+                        (name . "^\\.bbdb$")
+                        (name . "^\\.newsrc-dribble")))))))
+
+(add-hook 'ibuffer-mode-hook
+          (lambda ()
+            (ibuffer-switch-to-saved-filter-groups "default")))
+
+
 ;; install rudel
 ;; (load-file "~/.emacs.d/vendor/rudel-0.2-4/rudel-loaddefs.el")
 ;; (load-file "~/Desktop/bzr/rudel/rudel-loaddefs.el")
