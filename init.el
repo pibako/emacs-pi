3bc1220cb8a6bd6c29913b215adbd7c79cc723dc
Piotr Kowalski <piotr.kowalski@me.com>
2013-01-10 12:52:16 +0100

    Format machine generated xml... simple and clever.

--- init.el
+++ init.el
@@ -594,3 +594,18 @@ file of a buffer in an external program."
 (autoload 'smalltalk-mode "smalltalk-mode.elc" "Smalltalk mode" t)
 
 (autoload 'smalltalk-mode "gst-mode.elc" "GNU Smalltalk mode" t)
+
+(defun bf-pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+    (nxml-mode)
+    (goto-char begin)
+    (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+      (backward-char) (insert "\n"))
+    (indent-region begin end))
+  (message "Ah, much better!"))
