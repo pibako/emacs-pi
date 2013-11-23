edda91f9b815f3ef34763539fe3fa713185e5308
Piotr Kowalski <piotr.kowalski@me.com>
2013-11-23 17:38:42 +0100

    Fixed ctags.

--- init.d/ctags.el
+++ init.d/ctags.el
@@ -1,6 +1,10 @@
+(require 'ctags)
+(setq ctags-command "/usr/local/bin/ctags -e -R ")
+(setq tags-revert-without-query 1)      ; reload tags without yes-no query
+(global-set-key (kbd "<f7>") 'ctags-create-or-update-tags-table)
+
 ;; Enable ctags autoupdate
 (autoload 'turn-on-ctags-auto-update-mode "ctags-update" "turn on `ctags-auto-update-mode'." t)
-(setq tags-revert-without-query 1)      ; reload tags without yes-no query
 (add-hook 'c-mode-common-hook  'turn-on-ctags-auto-update-mode)
 (add-hook 'emacs-lisp-mode-hook  'turn-on-ctags-auto-update-mode)
 (add-hook 'ruby-mode-hook  'turn-on-ctags-auto-update-mode)
