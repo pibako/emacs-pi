41ca36baf437a453ebc50ba0880cbf80bc72ab25
Piotr Kowalski <piotr.kowalski@me.com>
2013-11-24 12:34:01 +0100

    Automatically manage packages.

--- init.d/01-packages.el
+++ init.d/01-packages.el
@@ -1,2 +1,5 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
+(require 'pallet)                       ; all new packages will
+                                        ; automatically be added or
+                                        ; removed to cask file
