a480c6493c7b41b48162bdf119a5a0d7cc0d22bf
Piotr Kowalski <piotr.kowalski@me.com>
2013-11-28 15:23:25 +0100

    Do not untabify in modes relaying on tabs.

--- init.d/programming.el
+++ init.d/programming.el
@@ -2,7 +2,14 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (defun untabify-hook ()
-  (untabify (point-min) (point-max)))
+  (let ((tab-sensitive-modes '(makefile-mode
+                               makefile-automake-mode
+                               makefile-gmake-mode
+                               makefile-imake-mode
+                               makefile-makepp-mode
+                               makefile-bsdmake-mode)))
+    (if (not (member major-mode tab-sensitive-modes))
+        (untabify (point-min) (point-max)))))
 
 (add-hook 'before-save-hook 'untabify-hook)
 
